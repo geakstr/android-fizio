10a7d1be46f0ac37a67c20abef0ade415076c324
37pin <ucorpor@gmail.com>
2015-10-12 16:27:49 +0300

    Added statistics activity

--- app/src/main/java/com/example/ucorp/fizioapp/MainActivity.java
+++ app/src/main/java/com/example/ucorp/fizioapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.ucorp.fizioapp;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -60,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), StatisticsActivity.class);
+                startActivity(intent);
             }
         });
 
