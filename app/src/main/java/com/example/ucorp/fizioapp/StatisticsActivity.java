5d5cc7ee1c94916ef916db9e1c10c39a171835f4
37pin <ucorpor@gmail.com>
2015-10-16 17:17:37 +0300

    Added chart

--- app/src/main/java/com/example/ucorp/fizioapp/StatisticsActivity.java
+++ app/src/main/java/com/example/ucorp/fizioapp/StatisticsActivity.java
@@ -1,25 +1,107 @@
 package com.example.ucorp.fizioapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
 
     ListView exerciseList;
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
         exerciseList = (ListView) findViewById(R.id.exerciseList);
-
         List<String> exercises = Arrays.asList("Exercise one", "Exercise two", "Exercise three");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exercises);
+
+        mChart = (LineChart) findViewById(R.id.chart);
+        mChart.setDrawGridBackground(false);
+
+        // no description text
+        mChart.setDescription("");
+        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setPinchZoom(true);
+
+        LimitLine llXAxis = new LimitLine(10f, "Index 10");
+        llXAxis.setLineWidth(4f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.setAxisMaxValue(220f);
+        leftAxis.setAxisMinValue(-50f);
+        leftAxis.setStartAtZero(false);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawLimitLinesBehindData(true);
+        mChart.getAxisRight().setEnabled(false);
+        setData(45, 100);
+        mChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
         exerciseList.setAdapter(adapter);
     }
+
+    private void setData(int count, float range) {
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.parseColor("#ffa500"));
+        set1.setCircleColor(Color.parseColor("#008000"));
+        set1.setLineWidth(1f);
+        set1.setCircleSize(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(Color.BLACK);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
+        mChart.setData(data);
+    }
+
 }
